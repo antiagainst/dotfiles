317cb173c4f964ad6862bf4ca5a4eb78403c4c79
Lei Zhang <antiagainst@gmail.com>
2017-01-20 13:49:44 -0500

    Tweak powerline segments and update install scripts about paths.

--- install.sh
+++ install.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
 ln -sf $(pwd)/dircolors         $HOME/.dircolors
-ln -sf $(pwd)/grc               $HOME/.grc
 ln -sf $(pwd)/gitconfig         $HOME/.gitconfig
 ln -sf $(pwd)/hgrc              $HOME/.hgrc
 ln -sf $(pwd)/tmux.conf         $HOME/.tmux.conf
 ln -sf $(pwd)/vimrc             $HOME/.vimrc
 ln -sf $(pwd)/ycm_extra_conf.py $HOME/.ycm_extra_conf.py
 
+[ -d $HOME/.grc ] && rm -rf $HOME/.grc
+ln -sf $(pwd)/grc           $HOME/.grc
+[ ! -d $HOME/.config/nvim ] && mkdir -p $HOME/.config/nvim
+ln -sf $(pwd)/vimrc                     $HOME/.config/nvim/init.vim
+
+[ ! -d $HOME/.config/powerline/themes/tmux ] && mkdir -p $HOME/.config/powerline/themes/tmux
+ln -sf $(pwd)/config/powerline/themes/tmux/default.json  $HOME/.config/powerline/themes/tmux/default.json
+
 os=$(uname -s)
+
+fail() {
+  printf "%025s\n" "[failed]" "$@"
+  exit 1
+}
 
 if [ "$os" == "Linux" ]; then
   [ ! -d $HOME/.fonts ] && mkdir $HOME/.fonts
   cp $(pwd)/fonts/PowerlineSymbols.otf $HOME/.fonts/
 
-  [ ! -d $HOME/.config/nvim ] && mkdir -p $HOME/.config/nvim
-  ln -sf $(pwd)/vimrc $HOME/.config/nvim/init.vim
-
   release=$(lsb_release -i | cut -f2)
   if [ "$release" == "Ubuntu" ]; then
     ln -sf $(pwd)/tmux-ubuntu.conf  $HOME/.tmux-ubuntu.conf
-    [ ! -d $HOME/.config/fontconfig/fonts.conf ] && mkdir -p $HOME/.config/fontconfig/fonts.conf
+    [ ! -d $HOME/.config/fontconfig/fonts.conf ] &&  mkdir -p $HOME/.config/fontconfig/fonts.conf
     ln -sf $(pwd)/config/fontconfig/10-powerline-symbols.conf $HOME/.config/fontconfig/fonts.conf/10-powerline-symbols.conf
+
+    powerline_dir=$(pip show powerline-status | grep Location | cut -d\  -f2)
+    [ -z "$powerline_dir" ] && fail "powerline-status not installed"
+    rm -rf $HOME/.tmux-ubuntu.conf && cp $(pwd)/tmux-ubuntu.conf $HOME/.tmux-ubuntu.conf
+    echo "source $powerline_dir/powerline/bindings/tmux/powerline.conf" >> $HOME/.tmux-ubuntu.conf
+    echo "source $powerline_dir/powerline/bindings/tmux/powerline_tmux_2.1_plus.conf" >> $HOME/.tmux-ubuntu.conf
   elif [ "$release" != "Arch" ]; then
-    print "linux release not supported:" $release
-    exit 1
+    fail "linux release not supported:" $release
   fi
 elif [ "$os" == "Darwin" ]; then
   brew_root_dir=$(brew --env | grep CMAKE_PREFIX_PATH | cut -d\" -f2)
@@ -33,6 +47,5 @@ elif [ "$os" == "Darwin" ]; then
   echo "source $brew_root_dir/$powerline_dir/powerline.conf" >> $HOME/.tmux-osx.conf
   echo "source $brew_root_dir/$powerline_dir/powerline_tmux_2.1_plus.conf" >> $HOME/.tmux-osx.conf
 else
-  print "operating system not supported:" $os
-  exit 1
+  fail "operating system not supported:" $os
 fi
